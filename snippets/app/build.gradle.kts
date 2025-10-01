b23283e11d49b1492d1512a4f58d26e26142d8f3
Enrique López Mañas <eenriquelopez@gmail.com>
2025-10-01 12:18:37 +0200

    feat: snippets

--- snippets/app/build.gradle.kts
+++ snippets/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         applicationId = "com.google.places"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -67,7 +67,7 @@ dependencies {
 
     // Places and Maps SDKs
     // [START maps_android_places_upgrade_snippet]
-    implementation("com.google.android.libraries.places:places:4.4.1")
+    implementation("com.google.android.libraries.places:places:5.0.0")
     // [END maps_android_places_upgrade_snippet]
 }
 // [END maps_android_places_install_snippet]
